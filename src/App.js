c2e366883249953069dbb56431f679a770f49398
settix-dev <settimbalamech@gmail.com>
2022-03-09 19:49:54 +0300

    Modify routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 // import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './components/Header';
-import Home from './routes/Home';
-import Berries from './routes/Berries';
-import About from './routes/About';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import Berries from "./routes/Berries";
+import Berry from "./routes/Berry";
+import BerryItems from "./routes/BerryItems";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
+    <BrowserRouter>
+      <div className="container-fluid">
+      <Header />
+        {/* <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/berries">Berries</Link>
+          </li>
+          <li>
+            <Link to="/berry">Berry</Link>
+          </li>
+          <li>
+            <Link to="/berryItems">BerryItems</Link>
+          </li>
+        </ul> */}
+
+        <hr />
+
         <Routes>
-          <Route path='/' element={<Home />}>Home</Route>
-          <Route path='about' element={<About />}>About</Route>
-          <Route path='berries' element={<Berries />}>Berries</Route>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/berries" element={<Berries />}> </Route>
+          <Route path="/berry" element={<Berry />}></Route>
+          <Route path="/berryItems" element={<BerryItems />}></Route>
+            {/* <Route path=":berry" element={<Berry />}>
+              <Route path=":berryItems" element={<BerryItems />}></Route>
+            </Route> */}
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>
-      <h1>Resource List</h1>
-      <p>Welcome to our Poke Api</p>
-  </div>;
+  return (
+    <>
+      <h1>Home</h1>
+      <p>
+        Welcome to our Poke Api. Please head over to
+        <Link to="/berries">/berries</Link> to see our catalog.
+      </p>
+    </>
+  );
 };
 
 export default Home;
