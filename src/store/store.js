3f8f25e55658fcca9409b0d3b4ccb43dcfd898e7
settix-dev <settimbalamech@gmail.com>
2022-03-09 19:53:30 +0300

    Modify store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,13 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+// import rootReducer from "./reducers/berriesApiReducer";
 
 
 let middlewares = [thunk];
 const middleware = applyMiddleware(...middlewares);
+// const store = createStore(rootReducer, composeWithDevTools(middleware));
 
-export default createStore(composeWithDevTools(middleware));
+export default createStore(rootReducer, composeWithDevTools(middleware));
+
